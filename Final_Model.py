1a078b682b1e284fd172a6e4a0cf6d0d7431d39d
AndRemy <andreremy88@gmail.com>
2020-11-05 17:37:05 -0800

    Major Commit (Test Score 0.821) SupervisedLearning_Analysis.ipynb: Jupyter notebook describing the complete analysis made to create the machine learning regression model. utils\regression_utils.py: Library used to ease the execution of models. Final_Model.py: Update of the script that creates the final model and saves it in a [pickle file](model/ApprenticeChef_RegressionModel.pkl). model\ApprenticeChef_RegressionModel.pkl: Pickle file that contains the final model serialized.

--- Final_Model.py
+++ Final_Model.py
@@ -1,11 +1,3 @@
-# timeit
-
-# Student Name : Marco Andre Remy Silva
-# Cohort       : 2 - Haight
-
-# Note: You are only allowed to submit ONE final model for this assignment.
-
-
 ################################################################################
 # Import Packages
 ################################################################################
@@ -13,469 +5,102 @@
 # use this space for all of your package imports
 import numpy as np
 import pandas as pd
+import pickle
 
-from sklearn.linear_model import Lars
-from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LassoCV
+from utils.regression_utils import Utils
 
 
 ################################################################################
 # Load Data
 ################################################################################
 
-# use this space to load the original dataset
-# MAKE SURE TO SAVE THE ORIGINAL FILE AS original_df
-# Example: original_df = pd.read_excel('Apprentice Chef Dataset.xlsx')
+# Loading data
+data_df = pd.read_excel("data/Apprentice_Chef_Dataset.xlsx")
 
-original_df = pd.read_excel("./data/Apprentice_Chef_Dataset.xlsx")
-
+# Removing inconsistencies
+data_df = data_df[data_df['LARGEST_ORDER_SIZE'] > 0]
 
 ################################################################################
 # Feature Engineering and (optional) Dataset Standardization
 ################################################################################
 
-# use this space for all of the feature engineering that is required for your
-# final model
+# Transforming dependent features
+data_df['ln_REVENUE'] = np.log(data_df['REVENUE'])
 
-# if your final model requires dataset standardization, do this here as well
 
-# FLAGGING OUTLIERS
+# Transforming independent features
+data_df['ln_TOTAL_PHOTOS_VIEWED'] = data_df['TOTAL_PHOTOS_VIEWED']
+data_df.loc[data_df['TOTAL_PHOTOS_VIEWED'] != 0, 'ln_TOTAL_PHOTOS_VIEWED'] = data_df['TOTAL_PHOTOS_VIEWED'].apply(np.log)
 
-# CROSS_SELL_SUCCESS
-# NO OUTLIER
+data_df['ln_UNIQUE_MEALS_PURCH'] = data_df['UNIQUE_MEALS_PURCH']
+data_df.loc[data_df['UNIQUE_MEALS_PURCH'] != 0, 'ln_UNIQUE_MEALS_PURCH'] = data_df['UNIQUE_MEALS_PURCH'].apply(np.log)
 
+data_df['ln_AVG_PREP_VID_TIME'] = data_df['AVG_PREP_VID_TIME']
+data_df.loc[data_df['AVG_PREP_VID_TIME'] != 0, 'ln_AVG_PREP_VID_TIME'] = data_df['AVG_PREP_VID_TIME'].apply(np.log)
 
-# TOTAL_MEALS_ORDERED
-out_total_meals_ordered = 350
+data_df['ln_TOTAL_MEALS_ORDERED'] = data_df['TOTAL_MEALS_ORDERED']
+data_df.loc[data_df['TOTAL_MEALS_ORDERED'] != 0, 'ln_TOTAL_MEALS_ORDERED'] = data_df['TOTAL_MEALS_ORDERED'].apply(np.log)
 
-original_df['out_TOTAL_MEALS_ORDERED'] = 0
-condition = original_df.loc[0:, 'out_TOTAL_MEALS_ORDERED'][original_df['TOTAL_MEALS_ORDERED'] > out_total_meals_ordered]
-original_df['out_TOTAL_MEALS_ORDERED'].replace(
-    to_replace=condition,
-    value=1,
-    inplace=True
-)
 
+# Flagging trends
+data_df['tre_AVG_CLICKS_PER_VISIT'] = 0
+data_df.loc[data_df['AVG_CLICKS_PER_VISIT'] > 11, 'tre_AVG_CLICKS_PER_VISIT'] = 1
 
-# UNIQUE_MEALS_PURCH
-out_unique_meals_purch = 12
+data_df['tre_LARGEST_ORDER_SIZE'] = 0
+data_df.loc[data_df['LARGEST_ORDER_SIZE'] > 8, 'tre_LARGEST_ORDER_SIZE'] = 1
 
-original_df['out_UNIQUE_MEALS_PURCH'] = 0
-condition = original_df.loc[0:, 'out_UNIQUE_MEALS_PURCH'][original_df['UNIQUE_MEALS_PURCH'] > out_unique_meals_purch]
-original_df['out_UNIQUE_MEALS_PURCH'].replace(
-    to_replace=condition,
-    value=1,
-    inplace=True
-)
+data_df['tre_MEDIAN_MEAL_RATING'] = 0
+data_df.loc[data_df['MEDIAN_MEAL_RATING'] > 3, 'tre_MEDIAN_MEAL_RATING'] = 1
 
+data_df['tre_CONTACTS_W_CUSTOMER_SERVICE'] = 0
+data_df.loc[data_df['CONTACTS_W_CUSTOMER_SERVICE'] > 10, 'tre_CONTACTS_W_CUSTOMER_SERVICE'] = 1
 
-# CONTACTS_W_CUSTOMER_SERVICE
-out_contacts_w_customer_service = 12.5
+# New features
+data_df['SOME'] = 0
+data_df.loc[
+    (data_df['WEEKLY_PLAN'] > 0) & (data_df['WEEKLY_PLAN'] < 15),
+    'SOME'
+] = 1
 
-original_df['out_CONTACTS_W_CUSTOMER_SERVICE'] = 0
-condition = original_df.loc[0:, 'out_CONTACTS_W_CUSTOMER_SERVICE'][
-    original_df['CONTACTS_W_CUSTOMER_SERVICE'] > out_contacts_w_customer_service
-]
-original_df['out_CONTACTS_W_CUSTOMER_SERVICE'].replace(
-    to_replace=condition,
-    value=1,
-    inplace=True
-)
-
-
-# PRODUCT_CATEGORIES_VIEWED
-# NO OUTLIER
-
-
-# AVG_TIME_PER_SITE_VISIT
-out_avg_time_per_site_visit = 190
-
-original_df["out_AVG_TIME_PER_SITE_VISIT"] = 0
-condition = original_df.loc[0:, 'out_AVG_TIME_PER_SITE_VISIT'][
-    original_df['AVG_TIME_PER_SITE_VISIT'] > out_avg_time_per_site_visit
-]
-original_df['out_AVG_TIME_PER_SITE_VISIT'].replace(
-    to_replace=condition,
-    value=1,
-    inplace=True
-)
-
-
-# MOBILE_NUMBER
-# NO OUTLIER
-
-
-# CANCELLATIONS_BEFORE_NOON
-out_cancellation_before_noon = 5
-
-original_df["out_CANCELLATIONS_BEFORE_NOON"] = 0
-condition = original_df.loc[0:, 'out_CANCELLATIONS_BEFORE_NOON'][
-    original_df['CANCELLATIONS_BEFORE_NOON'] > out_cancellation_before_noon
-]
-original_df['out_CANCELLATIONS_BEFORE_NOON'].replace(
-    to_replace=condition,
-    value=1,
-    inplace=True
-)
-
-
-# CANCELLATIONS_AFTER_NOON
-out_cancellation_after_noon = 0
-
-original_df["out_CANCELLATIONS_AFTER_NOON"] = 0
-condition = original_df.loc[0:, 'out_CANCELLATIONS_AFTER_NOON'][
-    original_df['CANCELLATIONS_AFTER_NOON'] > out_cancellation_after_noon
-]
-original_df['out_CANCELLATIONS_AFTER_NOON'].replace(
-    to_replace=condition,
-    value=1,
-    inplace=True
-)
-
-
-# TASTES_AND_PREFERENCES
-# NO OUTLIER
-
-
-# MOBILE_LOGINS
-# NO OUTLIER
-
-
-# PC_LOGINS
-# NO OUTLIER
-
-
-# WEEKLY_PLAN
-out_weekly_plan = 31
-
-original_df["out_WEEKLY_PLAN"] = 0
-condition = original_df.loc[0:, 'out_WEEKLY_PLAN'][original_df['WEEKLY_PLAN'] > out_weekly_plan]
-original_df['out_WEEKLY_PLAN'].replace(
-    to_replace=condition,
-    value=1,
-    inplace=True
-)
-
-
-# EARLY_DELIVERIES
-out_early_deliveries = 7
-
-original_df["out_EARLY_DELIVERIES"] = 0
-condition = original_df.loc[0:, 'out_EARLY_DELIVERIES'][original_df['EARLY_DELIVERIES'] > out_early_deliveries]
-original_df['out_EARLY_DELIVERIES'].replace(
-    to_replace=condition,
-    value=1,
-    inplace=True
-)
-
-
-# LATE_DELIVERIES
-out_late_deliveries = 8
-
-original_df["out_LATE_DELIVERIES"] = 0
-condition = original_df.loc[0:, 'out_LATE_DELIVERIES'][original_df['LATE_DELIVERIES'] > out_late_deliveries]
-original_df['out_LATE_DELIVERIES'].replace(
-    to_replace=condition,
-    value=1,
-    inplace=True
-)
-
-
-# PACKAGE_LOCKER
-# NO OUTLIER
-
-
-# REFRIGERATED_LOCKER
-# NO OUTLIER
-
-
-# FOLLOWED_RECOMMENDATIONS_PCT
-# NO OUTLIER
-
-
-# AVG_PREP_VID_TIME
-out_avg_prep_vid_time = 260
-
-original_df["out_AVG_PREP_VID_TIME"] = 0
-condition = original_df.loc[0:, 'out_AVG_PREP_VID_TIME'][original_df['AVG_PREP_VID_TIME'] > out_avg_prep_vid_time]
-original_df['out_AVG_PREP_VID_TIME'].replace(
-    to_replace=condition,
-    value=1,
-    inplace=True
-)
-
-
-# LARGEST_ORDER_SIZE
-out_largest_order_size = 8
-
-original_df["out_LARGEST_ORDER_SIZE"] = 0
-condition = original_df.loc[0:, 'out_LARGEST_ORDER_SIZE'][original_df['LARGEST_ORDER_SIZE'] > out_largest_order_size]
-original_df['out_LARGEST_ORDER_SIZE'].replace(
-    to_replace=condition,
-    value=1,
-    inplace=True
-)
-
-
-# MASTER_CLASSES_ATTENDED
-# NO OUTLIER
-
-
-# MEDIAN_MEAL_RATING
-# NO OUTLIER
-
-
-# AVG_CLICKS_PER_VISIT
-out_avg_clicks_per_visit = 8
-
-original_df["out_AVG_CLICKS_PER_VISIT"] = 0
-condition = original_df.loc[0:, 'out_AVG_CLICKS_PER_VISIT'][
-    original_df['AVG_CLICKS_PER_VISIT'] < out_avg_clicks_per_visit]
-
-original_df['out_AVG_CLICKS_PER_VISIT'].replace(
-    to_replace=condition,
-    value=1,
-    inplace=True
-)
-
-
-# TOTAL_PHOTOS_VIEWED
-out_total_photos_viewed = 420
-
-original_df["out_TOTAL_PHOTOS_VIEWED"] = 0
-condition = original_df.loc[0:, 'out_TOTAL_PHOTOS_VIEWED'][original_df['TOTAL_PHOTOS_VIEWED'] > out_total_photos_viewed]
-original_df['out_TOTAL_PHOTOS_VIEWED'].replace(
-    to_replace=condition,
-    value=1,
-    inplace=True
-)
-
-
-# FLAGGING TRENDS
-
-# CROSS_SELL_SUCCESS
-# NO TREND
-
-# TOTAL_MEALS_ORDERED
-tot_meals_seg_change = 20
-
-original_df['TOTAL_MEALS_ORDERED_change'] = 0
-condition = original_df.loc[0:, 'TOTAL_MEALS_ORDERED_change'][original_df['TOTAL_MEALS_ORDERED'] < tot_meals_seg_change]
-original_df['TOTAL_MEALS_ORDERED_change'].replace(
-    to_replace=condition,
-    value=1,
-    inplace=True
-)
-
-
-# UNIQUE_MEALS_PURCH
-unique_meals_purch_change = 1
-
-original_df['UNIQUE_MEALS_PURCH_change'] = 0
-condition = original_df.loc[0:, 'UNIQUE_MEALS_PURCH_change'][
-    original_df['UNIQUE_MEALS_PURCH'] <= unique_meals_purch_change
-]
-original_df['UNIQUE_MEALS_PURCH_change'].replace(
-    to_replace=condition,
-    value=1,
-    inplace=True
-)
-
-
-# CONTACTS_W_CUSTOMER_SERVICE
-contacts_w_customer_service_change = 10
-
-original_df['CONTACTS_W_CUSTOMER_SERVICE_change'] = 0
-condition = original_df.loc[0:, 'CONTACTS_W_CUSTOMER_SERVICE_change'][
-    original_df['CONTACTS_W_CUSTOMER_SERVICE'] <= contacts_w_customer_service_change
-]
-original_df['CONTACTS_W_CUSTOMER_SERVICE_change'].replace(
-    to_replace=condition,
-    value=1,
-    inplace=True
-)
-
-
-# PRODUCT_CATEGORIES_VIEWED
-# NO TREND
-
-
-# AVG_TIME_PER_SITE_VISIT
-# NO TREND
-
-# MOBILE_NUMBER
-# NO TREND
-
-
-# CANCELLATIONS_BEFORE_NOON
-# NO TREND
-
-
-# CANCELLATIONS_AFTER_NOON
-# NO TREND
-
-
-# TASTES_AND_PREFERENCES
-# NO TREND
-
-
-# MOBILE_LOGINS
-# NO TREND
-
-
-# PC_LOGINS
-# NO TREND
-
-# WEEKLY_PLAN
-# NO TREND
-
-
-# EARLY_DELIVERIES
-# NO TREND
-
-
-# LATE_DELIVERIES
-# NO TREND
-
-
-# PACKAGE_LOCKER
-# NO TREND
-
-
-# REFRIGERATED_LOCKER
-# NO TREND
-
-
-# FOLLOWED_RECOMMENDATIONS_PCT
-# NO TREND
-
-
-# AVG_PREP_VID_TIME
-# NO TREND
-
-
-# LARGEST_ORDER_SIZE
-# NO TREND
-
-
-# MASTER_CLASSES_ATTENDED
-master_classes_attended_change = 2
-
-original_df["MASTER_CLASSES_ATTENDED_change"] = 0
-condition = original_df.loc[0:, 'MASTER_CLASSES_ATTENDED_change'][
-    original_df['MASTER_CLASSES_ATTENDED'] > master_classes_attended_change
-]
-original_df['MASTER_CLASSES_ATTENDED_change'].replace(
-    to_replace=condition,
-    value=1,
-    inplace=True
-)
-
-
-# MEDIAN_MEAL_RATING#MEDIAN_MEAL_RATING
-out_median_meal_rating = 2
-
-original_df["out_MEDIAN_MEAL_RATING"] = 0
-condition = original_df.loc[0:, 'out_MEDIAN_MEAL_RATING'][original_df['MEDIAN_MEAL_RATING'] > out_median_meal_rating]
-original_df['out_MEDIAN_MEAL_RATING'].replace(
-    to_replace=condition,
-    value=1,
-    inplace=True
-)
-
-
-# AVG_CLICKS_PER_VISIT
-# NO TREND
-
-
-# TOTAL_PHOTOS_VIEWED
-total_photos_viewed_change = 0
-
-original_df['TOTAL_PHOTOS_VIEWED_change_1'] = 0
-condition = original_df.loc[0:, 'TOTAL_PHOTOS_VIEWED_change_1'][
-    original_df['TOTAL_PHOTOS_VIEWED'] == total_photos_viewed_change
-]
-original_df['TOTAL_PHOTOS_VIEWED_change_1'].replace(
-    to_replace=condition,
-    value=1,
-    inplace=True
-)
-
-total_photos_viewed_change = 1000
-
-original_df['TOTAL_PHOTOS_VIEWED_change_2'] = 0
-condition = original_df.loc[0:, 'TOTAL_PHOTOS_VIEWED_change_2'][
-    original_df['TOTAL_PHOTOS_VIEWED'] > total_photos_viewed_change
-]
-original_df['TOTAL_PHOTOS_VIEWED_change_2'].replace(
-    to_replace=condition,
-    value=1,
-    inplace=True
-)
-
-
-# DROPPING REVENUE OUTLIERS
-original_df = original_df[original_df["REVENUE"] < 6000]
-
-# MAKING A LOGARITHMIC TRANSFORMATION TO IMPROVE PREDICTION
-original_df["log_REVENUE"] = np.log(original_df["REVENUE"])
+data_df['SINGLE_MEAL'] = 0
+data_df.loc[data_df['UNIQUE_MEALS_PURCH'] == 1, 'SINGLE_MEAL'] = 1
 
 ################################################################################
 # Train/Test Split
 ################################################################################
 
-# use this space to set up testing and validation sets using train/test split
-
-# Note: Be sure to set test_size = 0.25
-
-SEED = 222
-TEST_SIZE = 0.25
-
-x_variables = [
-    'TOTAL_MEALS_ORDERED',                 
-    'UNIQUE_MEALS_PURCH',                  
-    'CONTACTS_W_CUSTOMER_SERVICE',         
-    'AVG_TIME_PER_SITE_VISIT',             
-    'PC_LOGINS',                           
-    'WEEKLY_PLAN',                         
-    'PACKAGE_LOCKER',                      
-    'AVG_PREP_VID_TIME',                   
-    'LARGEST_ORDER_SIZE',                  
-    'MASTER_CLASSES_ATTENDED',             
-    'MEDIAN_MEAL_RATING',                  
-    'TOTAL_PHOTOS_VIEWED',                 
-    
-    # Outliers
-    'out_AVG_TIME_PER_SITE_VISIT',         
-    'out_AVG_PREP_VID_TIME',               
-    'out_LARGEST_ORDER_SIZE',              
-    'out_AVG_CLICKS_PER_VISIT',            
-    'out_TOTAL_PHOTOS_VIEWED',             
-    'out_MEDIAN_MEAL_RATING',
-    
-    # Trends
-    'TOTAL_MEALS_ORDERED_change',          
-    'UNIQUE_MEALS_PURCH_change',           
-    'CONTACTS_W_CUSTOMER_SERVICE_change',  
-    'MASTER_CLASSES_ATTENDED_change',      
-    'TOTAL_PHOTOS_VIEWED_change_1',        
-    'TOTAL_PHOTOS_VIEWED_change_2'         
+features=[
+    'tre_AVG_CLICKS_PER_VISIT',
+    'ln_TOTAL_PHOTOS_VIEWED',
+    'ln_UNIQUE_MEALS_PURCH',
+    'SOME',
+    'tre_LARGEST_ORDER_SIZE',
+    'LARGEST_ORDER_SIZE',
+    'CONTACTS_W_CUSTOMER_SERVICE',
+    'MASTER_CLASSES_ATTENDED',
+    'MEDIAN_MEAL_RATING',
+    'ln_TOTAL_MEALS_ORDERED',
+    'tre_MEDIAN_MEAL_RATING',
+    'tre_CONTACTS_W_CUSTOMER_SERVICE',
+    'SINGLE_MEAL',
+    'ln_AVG_PREP_VID_TIME'
 ]
-
-X = original_df.loc[:, x_variables]
-y = original_df.loc[:, "log_REVENUE"]
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=TEST_SIZE, random_state=SEED)
-
+transformed_dependent = 'ln_REVENUE'
 
 ################################################################################
 # Final Model (instantiate, fit, and predict)
 ################################################################################
 
-# use this space to instantiate, fit, and predict on your final model
+# Model creation
+final_model = LassoCV(eps=0.00092)
 
-final_model = Lars()
-final_model.fit(X_train, y_train)
-y_predict = final_model.predict(X_test)
+model_result = Utils.split_execute_model(
+    x_col=features,
+    y_col=transformed_dependent,
+    data=data_df,
+    model=final_model
+)
 
 
 ################################################################################
@@ -486,4 +111,12 @@ y_predict = final_model.predict(X_test)
 # MAKE SURE TO SAVE YOUR TEST SCORE AS test_score
 # Example: test_score = final_model.score(X_test, y_test)
 
-test_score = final_model.score(X_test, y_test)
+test_score = model_result['test_score']
+print(f"Test Score: {test_score}")
+
+################################################################################
+# Serializing Object
+################################################################################
+
+with open("model/ApprenticeChef_RegressionModel.pkl", "wb") as file:
+    pickle.dump(final_model, file)
--- utils/regression_utils.py
+++ utils/regression_utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
@@ -13,25 +14,27 @@ class Utils:
     __SEED = 222
 
     @classmethod
-    def execute_model(
+    def split_execute_model(
         cls,
         x_col: list,
         y_col: str,
         data: pd.DataFrame,
         model: object = LinearRegression(),
         standardize: bool = False
-    ):
+    ) -> dict:
         """
         Executes a model by separating the data in TRAIN and TEST set. It prints the train and test score of the
-        created model. Since the model is sent as an object (by reference) to this method, the detailes of the created
+        created model. Since the model is sent as an object (by reference) to this method, the details of the created
         model can be accessed by the program that calls this method.
+        
         :param x_col: Name of the columns that conforms the predictors
         :param y_col: Name of the column that will be predicted by the model
         :param data: Data set with the data to train and test the model.
         :param model: Name of the model to run. By default it will use run the OLS model
-        (scikit-learn's LinearRegressison).
+        (scikit-learn's LinearRegression).
         :param standardize: True if you want this method to standardize the data, False if not. Default value is False.
-        :return: Doesn't return any information.
+
+        :return: Returns a dictionary with the Train score (train_score), Test score (test_score), and RSME (rsme).
         """
 
         x_df = data.loc[:, x_col]
@@ -51,10 +54,45 @@ class Utils:
         model.fit(x_train, y_train)
         y_predict = model.predict(x_test)
 
-        train_score = model.score(x_train, y_train)
-        test_score = model.score(x_test, y_test)
-        rsme = mean_squared_error(y_test, y_predict, squared=False)  # math.sqrt(np.mean((y_predict - y_test) ** 2))
+        train_score = np.round(model.score(x_train, y_train), decimals=3)
+        test_score = np.round(model.score(x_test, y_test), decimals=3)
+        rsme = np.round(mean_squared_error(y_test, y_predict, squared=False), decimals=3)
+        # rsme = math.sqrt(np.mean((y_predict - y_test) ** 2))
 
-        print(f"Train Score: {train_score}")
-        print(f"Test Score: {test_score}")
-        print(f"RSME: {rsme}")
+        return {
+            "train_score": train_score,
+            "test_score": test_score,
+            "rsme": rsme
+        }
+
+    @classmethod
+    def execute_model(
+        cls,
+        x_train: list,
+        y_train: str,
+        x_test: pd.DataFrame,
+        y_test: pd.Series,
+        model: object = LinearRegression(),
+    ) -> dict:
+        """
+
+        :param x_train:
+        :param y_train:
+        :param x_test:
+        :param y_test:
+        :param model:
+        :return: Returns a dictionary with the Train score (train_score), Test score (test_score), and RSME (rsme).
+        """
+
+        model.fit(x_train, y_train)
+        y_predict = model.predict(x_test)
+
+        train_score = np.round(model.score(x_train, y_train), decimals=3)
+        test_score = np.round(model.score(x_test, y_test), decimals=3)
+        rsme = np.round(mean_squared_error(y_test, y_predict, squared=False), decimals=3)
+
+        return {
+            "train_score": train_score,
+            "test_score": test_score,
+            "rsme": rsme
+        }
